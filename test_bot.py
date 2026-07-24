78043d48839c3b9c482185dffe0b649223dece35
ogonowskilukasz5-prog <ogonowskilukasz5@gmail.com>
2026-07-24 12:41:02 +0200

    Create test_bot.py

---
+++ test_bot.py
@@ -0,0 +1,18 @@
+import os
+import asyncio
+from telegram import Bot
+
+TOKEN = os.getenv("TELEGRAM_TOKEN")
+CHAT_ID = os.getenv("CHAT_ID")
+
+
+async def main():
+    bot = Bot(token=TOKEN)
+
+    await bot.send_message(
+        chat_id=CHAT_ID,
+        text="✅ Bybit Smart Scanner działa!"
+    )
+
+
+asyncio.run(main())
